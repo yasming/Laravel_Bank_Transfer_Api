442874351cc33fccf7d2baacc548c202e7c2176c
Yasmin <yasmin@lit.ifce.edu.br>
2021-07-26 18:17:01 -0300

    creating balance table, migrations and user seed

---
+++ database/migrations/2021_07_26_210658_create_balances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBalancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('balances', function (Blueprint $table) {
+            $table->id();
+            $table->float('amount');
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('balances');
+    }
+}
