d9d1b42e54da9ce8c706916abd9991940c51264a
Yasmin <yasmin@lit.ifce.edu.br>
2021-07-26 18:30:05 -0300

    creating transfer model

---
+++ app/Models/Transfer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transfer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'payee_id',
+        'payer_id',
+        'amount'
+    ];
+}
---
+++ database/migrations/2021_07_26_211924_create_transfers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->float('amount');
+            $table->unsignedBigInteger('payee_id');
+            $table->unsignedBigInteger('payer_id');
+            $table->foreign('payee_id')->references('id')->on('users');
+            $table->foreign('payer_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+}
